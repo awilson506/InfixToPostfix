ee66ba13b3566333b4ece152c6ce0b9b9790ae19
Adam Wilson <awilson@pubsvs.com>
2014-11-07 16:15:14 -0500

    eval added

---
+++ src/Evaluate.java
@@ -0,0 +1,64 @@
+import java.util.StringTokenizer;
+
+
+public class Evaluate {
+	
+	private static final char ADD = '+', SUBTRACT = '-';
+    private static final char MULTIPLY = '*', DIVIDE = '/', POW = '^';
+	
+	static int calculate(String expr) { 
+		Stack stack = new Stack(0);
+	    int op1, op2, result = 0;
+	    String token;
+	    StringTokenizer tokenizer = new StringTokenizer(expr);
+	
+	    while (tokenizer.hasMoreTokens()) {
+	        token = tokenizer.nextToken();
+	
+	        if (isOperator(token)) {
+	            op2 = ((Integer) stack.pop()).intValue();
+	            op1 = ((Integer) stack.pop()).intValue();
+	            result = evalSingleOp(token.charAt(0), op1, op2);
+	            stack.push(new Integer(result));
+	        }
+	        else
+	            stack.push(new Integer(Integer.parseInt(token)));
+	    }
+	
+	    result = ((Integer) stack.pop()).intValue();
+	    return result;
+	}  
+	
+	private static boolean isOperator(String token) {
+	    return (
+	        token.equals("+")
+	            || token.equals("-")
+	            || token.equals("*")
+	            || token.equals("/"));
+	}
+	
+	private static int evalSingleOp(char operation, int op1, int op2) {
+        double result = 0;
+
+        switch (operation) {
+            case ADD :
+                result = op1 + op2;
+                break;
+            case SUBTRACT :
+                result = op1 - op2;
+                break;
+            case MULTIPLY :
+                result = op1 * op2;
+                break;
+            case DIVIDE :
+                result = op1 / op2;
+                break;
+            case POW : 
+            	result = (int) Math.pow(op1,op2);
+        }
+        
+        System.out.println(result);
+
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
 	public static void main(String[] args) {
-       //String infix = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
+       String infix = "3 + 4 * 2 / ( 1 - 5 ) + 2 ^ 3";
         //String infix = "(5 + 3) - 10*(5- 3 ) + 75 / 5 ^ 2 x ( 2 + 3 )";
-		String infix = "(5+ 3) * 2 - 10 + 4 * (4-1)";
+		//String infix = "(5+ 3) * 2 - 10 + 4 * (4-1)";
         System.out.printf("infix:   %s%n", infix);
         System.out.printf("postfix: %s%n", Convert.infixToPostfix(infix));
-        
+        System.out.println(Evaluate.calculate(Convert.infixToPostfix(infix)));
     }
 
 }
