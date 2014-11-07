fd8f268dc10f9815f79919c4db5b566d13c3e122
Adam Wilson <awilson@pubsvs.com>
2014-11-07 15:43:42 -0500

    spaceing

--- src/Convert.java
+++ src/Convert.java
@@ -1,6 +1,5 @@
 
 public class Convert {
-	
  
     static String infixToPostfix(String infix) {
     	
@@ -11,37 +10,47 @@ public class Convert {
         
         for (String token : tokenized ) {
         	
-        	if(token.trim().length() > 0){
+        	if(token.trim().length() > 0) {
+        		
 	            char c = token.charAt(0);
 	            int current = operators.indexOf(token);
 	            if (current != -1 && token.length() == 1) {
+	            	
 	                if (stack.isEmpty()){
 	                    stack.push(current);
 	                }
 	                else {
+	                	
 	                    while (!stack.isEmpty()) {
 	                        int prec2 = stack.peek() / 2;
 	                        int prec1 = current / 2;
 	                        if (prec2 > prec1 || (prec2 == prec1 && c != '^'))
 	                        	queue.enqueue(operators.charAt(stack.pop()));
-	                        else break;
+	                        else {
+	                        	break;
+	                        }
 	                    }
 	                    stack.push(current);
 	                }
 	            } else if (c == '(') {
+	            	
 	                stack.push(-2);
 	            } else if (c == ')') {
-	                while (stack.peek() != -2)
+	            	
+	                while (stack.peek() != -2) {
 	                	queue.enqueue(operators.charAt(stack.pop()));
+	                }
 	                stack.pop();
 	            } else {
+	            	
 	            	queue.enqueue(token);
 	            }
 	        }
         }
-        while (!stack.isEmpty())
+        while (!stack.isEmpty()) {
+        	
         	queue.enqueue(operators.charAt(stack.pop()));
-        
+        }
         return queue.elements();
     }
 }
