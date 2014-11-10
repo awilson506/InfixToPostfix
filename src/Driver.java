4f187ef50e5f090c0cbd3ddf714f518e2f95dd43
Adam Wilson <awilson@pubsvs.com>
2014-11-10 16:26:52 -0500

    added output file

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,27 @@
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 
 
 public class Driver {
 
 	public static void read(){
+		
 		try {
+			String infix;
+			String postfix;
 		    BufferedReader in = new BufferedReader(new FileReader("input.txt"));
-		    String infix;
+		    PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+		    System.setOut(out);
+		    
 		    while ((infix = in.readLine()) != null){
-		    	System.out.printf("infix:    %s%n", infix);
-		    	System.out.printf("postfix: %s%n", Convert.infixToPostfix(infix));
-		    	System.out.printf("Evaluation: %s%n" , Evaluate.calculate(Convert.infixToPostfix(infix)));
+		    	
+		    	System.out.printf("Infix:    %s%n", infix);
+		    	postfix = Convert.infixToPostfix(infix);
+		    	System.out.printf("Postfix: %s%n", postfix);
+		    	System.out.printf("Evaluation: %s%n" , Evaluate.calculate(postfix));
 		    	System.out.println("**********************************");
 		    }
 		    in.close();
